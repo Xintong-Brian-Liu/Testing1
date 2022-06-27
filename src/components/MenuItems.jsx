689437655e3bb6b528585556065473b833c0d5a1
Xintong-Brian-Liu <82603595+Xintong-Brian-Liu@users.noreply.github.com>
2022-06-27 10:13:52 -0700

    First Commit

---
+++ src/components/MenuItems.jsx
@@ -0,0 +1,34 @@
+import { useLocation } from "react-router";
+import { Menu } from "antd";
+import { NavLink } from "react-router-dom";
+
+function MenuItems() {
+  const { pathname } = useLocation();
+
+  return (
+    <Menu
+      theme="light"
+      mode="horizontal"
+      style={{
+        display: "flex",
+        fontSize: "17px",
+        fontWeight: "500",
+        width: "100%",
+        justifyContent: "center",
+      }}
+      defaultSelectedKeys={[pathname]}
+    >
+      <Menu.Item key="/nftMarket">
+        <NavLink to="/nftMarket">📄 Explore</NavLink>
+      </Menu.Item>
+      <Menu.Item key="/nft">
+        <NavLink to="/nftBalance">📄 Your Collections</NavLink>
+      </Menu.Item>
+      <Menu.Item key="/transactions">
+        <NavLink to="/transactions">📄 Transactions</NavLink>
+      </Menu.Item>
+    </Menu>
+  );
+}
+
+export default MenuItems;
